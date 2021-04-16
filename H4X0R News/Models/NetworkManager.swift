a94b905cd38a7bfd174cc785c793c93595de600b
mhabzda <marcin.habzda1@gmail.com>
2021-04-16 14:39:12 +0200

    improve NetworkManager

--- H4X0R News/Models/NetworkManager.swift
+++ H4X0R News/Models/NetworkManager.swift
@@ -16,21 +16,25 @@ class NetworkManager: ObservableObject {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error == nil {
-                    let decoder = JSONDecoder()
-                    if let safeData = data {
-                        do {
-                            let results = try decoder.decode(Results.self, from: safeData)
-                            DispatchQueue.main.async {
-                                self.posts = results.hits
-                            }
-                        } catch {
-                            print(error)
-                        }
-                    }
+                    self.decodeData(data)
                 }
             }
             
             task.resume()
         }
     }
-}
+    
+    private func decodeData(_ data: Data?) {
+        let decoder = JSONDecoder()
+        if let safeData = data {
+            do {
+                let results = try decoder.decode(Results.self, from: safeData)
+                DispatchQueue.main.async {
+                    self.posts = results.hits
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+ }
